61c95b368ff4a3e4ad3f3bbac2849b0137ea2664
Julia <jostroushko@gmail.com>
2019-04-17 22:51:40 +0700

    задание номер 3

--- script.js
+++ script.js
@@ -1,31 +1,108 @@
 'use strict';
-let money = prompt('Ваш бюджет на месяц?', 10000),
-    time = prompt('Введите дату в формате YYYY-MM-DD', 'YYY-MM-DD'),
-    st = prompt('Введите обязательную статью расходов в этом месяце', 'статья'),
-    hm = prompt('Во сколько обойдется?', 10000);
-var appData = {
-    money:money,
-    dateTime:time,
-    expenses:{},
-    optionalExpenses:{},
-    income:[],
-    savings:false
+
+
+let money = +prompt ("Ваш бюджет на месяц?", ""),
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+    
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
-appData.expenses.st=hm;
-var oneDay = (appData.money - appData.expenses.st)/30;
-alert('Расход на один день: '+oneDay);
-let x = 2 && 1 && null && 0 && undefined; 
-alert( !!(1 && 2 )); 
-let y = [ ] + false - null + true;
-console.log( y ); 
-let q = 1; let w = q = 2; alert('третий вопрос '+ w);
-let t=[ ] + 1 + 2; alert('четвертый вопрос '+ t);
-alert( "1"[0] );
-let a = [1, 2, 3], 
-    b = [1, 2, 3];
-    if ("Ёжик" > "яблоко"){
-alert( 'true');}else{
-    alert('false')
+
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt ("Во сколько обойдется?", "");
+
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+        console.log ("done");
+
+        appData.expenses[a] = b;
+    } else {                            
+        console.log ("bad result");
+        i--;
+    }
+
+};
+
+
+
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Это высокий уровень достатка!");
+} else {
+    console.log ("Произошла ошибка");
 }
-let p=0 || "" || 2 || undefined || true || falsе;
-alert(p );
+// let x = 2 && 1 && null && 0 && undefined; 
+// alert( !!(1 && 2 )); 
+// let y = [ ] + false - null + true;
+// console.log( y ); 
+// let q = 1; let w = q = 2; alert('третий вопрос '+ w);
+// let t=[ ] + 1 + 2; alert('четвертый вопрос '+ t);
+// alert( "1"[0] );
+// let a = [1, 2, 3], 
+//     b = [1, 2, 3];
+//     if ("Ёжик" > "яблоко"){
+// alert( 'true');}else{
+//     alert('false')
+// }
+// let p=0 || "" || 2 || undefined || true || falsе;
+// alert(p );
